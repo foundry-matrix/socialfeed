c200346793b63a44e8f310a55fd4864bcf6c2df5
perborgen <perhb@hotmail.com>
2015-02-13 13:49:56 +0000

    clean up code

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 var http = require('http');
 var twit = require('twitter');
-var st = require('node-static');
 var fs = require("fs");
 var index = fs.readFileSync("./index.html").toString();
-var ig = require('instagram-node').instagram();
-
-ig.use({ access_token: '38313696.8ac3169.2676a0eafcb447d391175e0cfec175e3'});
-ig.use({ client_id: 'ca838b85d5c84ca786d7aef7c92bb747',
-         client_secret: '7f14ce701bdf43be813695a10ecdc14a' });
+var url = require('url');
 
 var twitter = new twit({
 	consumer_key: "v1KUPLFkZdiu6BYjsrY98HpaA",
@@ -16,52 +11,25 @@ var twitter = new twit({
 	access_token_secret: "frg80VNHox5mhDds7yz7vf1y1awtjpmss5aepSSXn1bdK"
 });
 
-ig.location_search({ lat:51.529457, lng: -0.0423818}, function(err, result, remaining, limit) {
-	if (err){
-		console.log("Error");
-	}
-	else{
-		console.dir(result);
-
-	}
-	//console.log(result);
-});
-
-
-
-var url = require('url');
-
 http.createServer(function (req, res) {
-  console.log("URL:",req.url);
-  var word;
   if(req.url === '/'){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(index);
-} else {
-	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query["text"];
-	//console.dir(query);
-	var geocode = url_parts.query["geocode"];
-	//console.log(geocode);
-	if(query) {
-		  twitter.get('search/tweets', { count: 20, geocode: "40.759101,-73.984406,1mi"}, 
-		  function(error, tweets, response){
-		  res.writeHead(200, {'Content-Type': 'application/javascript'});
-		  res.end(JSON.stringify(tweets.statuses));
-	});
-	
-	}else if (geocode) {
-		twitter.get('search/tweets', {count:10, geocode: geocode}, 
+	} else {
+		var url_parts = url.parse(req.url, true);
+		var geocode = url_parts.query["geocode"];
+		} if (geocode) {
+			twitter.get('search/tweets', {count:10, geocode: geocode}, 
 			function(error,tweets,response){
 				res.writeHead(200,  {'Content-Type': 'application/javascript'});
 				res.end(JSON.stringify(tweets.statuses));
-
 			});
-
-	}
-	 else {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-    	res.end(index);
-	}
+		}
+	 	else {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+    		res.end(index);
+		}
 }
-}).listen(process.env.PORT || 3000);
+).listen(process.env.PORT || 3000);
+
+
