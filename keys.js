a726f58303b6ad52f9d2a1b9ce18b9bc1ab81923
warringgod <alastorlion@hotmail.co.uk>
2015-02-16 18:32:33 +0000

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-var secretkeys={
-consumer_key:'f1DcWeVtaYVPB4d1JjrSswzBE',
-consumer_secret: 'EVq4UyzyWOvd2d1sQLqW0xK7rK275axcMexWJ0R3zrl9gPOEpL',
-access_token_key: '31862927-pRNQrTm7JnDuoFzNaDTVhGc03Lo58ozqKGnlrWkDs',
-access_token_secret: '6VN7jmrgckTC8QpxQqRRkCEJrRGF57AXyB0MUOjHIr88t'
-}
-
-exports.secretkeys=secretkeys;
